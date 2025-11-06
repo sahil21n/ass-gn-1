213d57c3c74686dee6bc771a0fe417cb5b09fbdb
sahil21n <sahil.xb.21@gmail.com>
2025-11-06 09:09:40 +0530

    Update x1.cpp

--- ASS/ASSGN-6/x1.cpp
+++ ASS/ASSGN-6/x1.cpp
@@ -1,93 +1,42 @@
-#include <iostream>
-using namespace std;
-
-class node {
-public:
-    int val;
-    node* next;
-    node(int val) {
-        this->val = val;
-        next = NULL;
+Node* middle(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+        
+    while (fast->next != head && fast->next->next != head) {
+        fast = fast->next->next;
+        slow = slow->next;
     }
-};
-
-class linkedlist {
-public:
-    node* head;
-    linkedlist() {
-        head = NULL;
-    }
-
-    void insert(int val) {
-        node* n = new node(val);
-        if (head == NULL) {
-            head = n;
-            head->next = head; // circular
-            return;
-        }
-        node* temp = head;
-        while (temp->next != head) {
-            temp = temp->next;
-        }
-        temp->next = n;
-        n->next = head;
-    }
-
-    void split() {
-        if (head == NULL || head->next == head) {
-            cout << "Not enough nodes to split.";
-            return;
-        }
-
-        node* slow = head;
-        node* fast = head;
-
-        // find left middle
-        while (fast->next != head && fast->next->next != head) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        node* p = slow->next;   // start of 2nd half
-        slow->next = head;      // close first circle
-
-        node* temp = p;
-        while (temp->next != head) {  // find end of 2nd half
-            temp = temp->next;
-        }
-        temp->next = p;  // close second circle
-
-        cout << "First half: ";
-        display(head);
-        cout << "\nSecond half: ";
-        display(p);
-    }
-
-    void display(node* start) {
-        if (start == NULL) return;
-
-        node* temp = start;
-        cout << temp->val << " ";
-        temp = temp->next;
-
-        while (temp != start) {
-            cout << temp->val << " ";
-            temp = temp->next;
-        }
-    }
-};
-
-int main() {
-    linkedlist l;
-    l.insert(10);
-    l.insert(20);
-    l.insert(30);
-    l.insert(40);
-    l.insert(50);
-    l.insert(60);
-    l.insert(70);
-
-    l.split();
-
-    return 0;
+    return slow;       
 }
+
+Node* gettail(Node* head){
+    Node* temp = head;
+    while (temp->next != head) {
+        temp = temp->next;
+    }
+    return temp;
+}
+
+class Solution {
+public:
+    pair<Node*, Node*> splitList(Node *head) {
+
+        if (head == NULL || head->next == head) {
+            return {head, NULL}; // Only 0 or 1 node
+        }
+
+        // 1. Find mid (slow) and tail
+        Node* mid = middle(head);
+        Node* head2 = mid->next;
+        Node* tail = gettail(head);
+
+        // 2. Make first half circular
+        mid->next = head;
+
+        // 3. Make second half circular
+        tail->next = head2;
+
+        // 4. Return both heads
+        return {head, head2};
+    }
+};
